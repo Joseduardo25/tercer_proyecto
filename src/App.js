d29207aef176842852a669d4113c7778f1a72fb8
Eduardo Calachua <jocdwardo25@gmail.com>
2020-08-06 19:43:12 -0500

    material_UI_form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState }from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import { deepOrange } from '@material-ui/core/colors';
+
+import InputAdornment from '@material-ui/core/InputAdornment';
+import TextField from '@material-ui/core/TextField';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Button from '@material-ui/core/Button';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    },
+    orange: {
+      color: theme.palette.getContrastText(deepOrange[500]),
+      backgroundColor: deepOrange[500],
+    },
+    margin: {
+      margin: theme.spacing(1),
+    },
+    root_button: {
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    }
+  }));
+  const classes = useStyles();
+  const usuario = []
+  const[valueInput, setValueInput] = useState('')
+  const[array,setArray] = useState(usuario)
+  const onChangeTextField = (e) => {
+    setValueInput(e.target.value)
+  }
+  const onClickButton = () => {
+    const copyArray = [...array]
+    copyArray.push(valueInput)
+    setArray(copyArray)
+    setValueInput('')
+  }
+  const onSubmitForm = (e) => {
+    e.preventDefault()
+    onClickButton()
+  }
+
+    return (
+      <div>
+       <form onSubmit={onSubmitForm}>
+        <div className={classes.root}>
+          {array.map((e)=>(<Avatar alt={e} src={e} className={classes.orange} />))}
+        </div>
+        <TextField
+          className={classes.margin}
+          id="input-with-icon-textfield"
+          label="Nombre"
+          value={valueInput}
+          onChange={onChangeTextField}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <AccountCircle />
+              </InputAdornment>
+            ),
+          }}
+        />
+        <div className={classes.root_button}>
+          <Button onClick={onClickButton} variant="contained" color="primary" href="#contained-buttons">
+            Link
+          </Button>
+        </div>
+      </form>
+      </div>
+    );
+  
 }
 
 export default App;
